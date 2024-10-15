a4af437100037839b9f5c4832eb9952dc7dd7191
Vijay Kumar <Be1newinner@gmail.com>
2024-10-15 17:57:34 +0530

    adding JSON TOKEN

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 
 const express = require("express");
 const morgan = require("morgan");
-
+const path = require("path");
+const jwt = require('jsonwebtoken');
 const app = express();
 
 
 console.log(process.env.API)
 
-app.use(morgan())
+app.use(morgan("dev"))
 app.use(express.json());
 // app.use(express.static('static'))
+app.use(express.static(path.join(__dirname, 'build')));
 
 const users = [
     { id: 1, name: "John Doe", email: "john@example.com", password: "123" },
@@ -22,9 +24,30 @@ const users = [
 
 // app.use('/static', express.static('index.html'))
 
-app.get("/", (req, res) => {
-    console.log(req.query)
-    res.end(JSON.stringify({ name: "Vijay" }))
+app.all("/h2", (req, res) => {
+    // const apiKey = req.headers["x-api-key"];
+    // const myToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYmUxbmV3aW5uZXIiLCJpYXQiOjE3Mjg5OTM1NDF9.FYWJ0dD-DIOTU1hXWm-Bj09j6jWeyWDYXgud1waXkZg";
+    
+    // var token = jwt.sign({ foo: 'bar', exp: Math.floor(Date.now() / 1000) + (60 * 60), }, 'shhhhh');
+    
+    // const token = jwt.sign({ user: 'be1newinner' }, 'dasdasdasd8asd7sad8');
+
+    // var decoded = jwt.verify(token, 'shhhhh');
+    // console.log("DATA => ", token)
+    // console.log(decoded) 
+
+    res.status(200).cookie("access_token", "SOME_TOKEN", {
+        httpOnly: true,
+        secure: true,
+    })
+    res.end()
+
+    // res.end(token)
+    // if (apiKey === "da4T38fwe4fwDDW238") {
+    //     res.end(JSON.stringify({ name: "Vijay" }))
+    // } else {
+    //     res.end(JSON.stringify({ error: "Invalid Key!" }))
+    // }
 });
 
 app.get("/auth/:name", (req, res) => {
@@ -49,4 +72,8 @@ app.post("/login", (req, res) => {
     }
 })
 
-app.listen(3000)
+app.get('/file/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3001)
